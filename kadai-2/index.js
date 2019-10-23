fa6a3308573d11e8f9c43ab635a4fcb15771a2db
asuka02031d <asuka02031d@gmail.com>
2019-10-23 05:16:34 +0000

    Some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,15 @@
+const firstName = 'Asuka'
+const secondName = 'Tsuji'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5
+console.log(sum)
+
+
+const  hasCat = false
+const hasDog = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+//犬も猫も買っていない
+console.log(!hasDog&&!hasCat)
