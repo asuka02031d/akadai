25845adf618654039782256e72e1acba0c3b205a
asuka02031d <asuka02031d@gmail.com>
2019-11-06 05:27:27 +0000

    kadai-4

---
+++ kadai-4/index2.js
@@ -0,0 +1,13 @@
+//正解
+setTimeout(
+    () => {
+        console.log('hello!')
+        setTimeout(
+            () => {
+                console.log('bye!')
+            },
+            2000
+        )
+    },
+    3000
+)
